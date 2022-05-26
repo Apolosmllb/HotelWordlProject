34ecccd59022b8690d31ce17799a2b3f16ad12e9
Moisesmp75 <48933728+Moisesmp75@users.noreply.github.com>
2022-05-26 18:20:35 -0500

    Update chat-g.component.spec.ts

--- src/app/pages/components/chat-g/chat-g.component.spec.ts
+++ src/app/pages/components/chat-g/chat-g.component.spec.ts
@@ -1,1 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatGComponent } from './chat-g.component';
+
+describe('ChatGComponent', () => {
+  let component: ChatGComponent;
+  let fixture: ComponentFixture<ChatGComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ChatGComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChatGComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
 
