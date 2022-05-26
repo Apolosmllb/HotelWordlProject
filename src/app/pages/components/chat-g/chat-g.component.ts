0d00a6ed1f813f840fb315261501075f36e0f405
Moisesmp75 <48933728+Moisesmp75@users.noreply.github.com>
2022-05-26 18:20:13 -0500

    Update chat-g.component.ts

--- src/app/pages/components/chat-g/chat-g.component.ts
+++ src/app/pages/components/chat-g/chat-g.component.ts
@@ -1,1 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {ChatService} from '../../services/chat.service'
+
+@Component({
+  selector: 'app-chat-g',
+  templateUrl: './chat-g.component.html',
+  styleUrls: ['./chat-g.component.css']
+})
+export class ChatGComponent implements OnInit {
+  text="";
+  constructor(public chat:ChatService) { }
+
+  ngOnInit(): void {
+  }
+  sendMessage(){
+    let messageInfo = {
+      text: this.text,
+      messageType: 1
+    };
+    this.chat.sendMessage(messageInfo);
+    this.text = "";
+  }
+
+}
 
